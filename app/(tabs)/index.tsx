c5d0f61ff8945784f056ad4a3654c7372c0cafe3
Matheus Bernardino <mmtheus69@gmail.com>
2026-05-18 16:54:47 -0300

    Telas de slide inicial finalizada

--- app/(tabs)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function HomeScreen() {
-  return (
-    <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-2xl font-bold text-white">Ryde</Text>
-    </View>
-  );
-}
